0d6968ed8d3ccc854db710c9e723e521cf6bb665
Junan Lu <anplus@foxmail.com>
2025-02-26 21:57:17 +0800

    First commit

---
+++ src/main.rs
@@ -0,0 +1,87 @@
+mod example;
+
+use std::io;
+use std::net::ToSocketAddrs;
+use std::path::PathBuf;
+
+use argh::FromArgs;
+use rustls::pki_types::pem::PemObject;
+use rustls::pki_types::{CertificateDer, PrivateKeyDer};
+use std::error::Error as StdError;
+use std::sync::Arc;
+use tokio::io::{copy, split, AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_rustls::{rustls, TlsAcceptor};
+
+/// Simple https reverse proxy
+#[derive(FromArgs)]
+struct Options {
+    /// bind addr
+    #[argh(positional)]
+    addr: String,
+
+    /// target address
+    #[argh(option, short = 't')]
+    target: String,
+
+    /// cert file
+    #[argh(option, short = 'c')]
+    cert: PathBuf,
+
+    /// key file
+    #[argh(option, short = 'k')]
+    key: PathBuf,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn StdError + Send + Sync + 'static>> {
+    let options: Options = argh::from_env();
+
+    let addr = options
+        .addr
+        .to_socket_addrs()?
+        .next()
+        .ok_or_else(|| io::Error::from(io::ErrorKind::AddrNotAvailable))?;
+    let target_addr = options
+        .target
+        .to_socket_addrs()?
+        .next()
+        .ok_or_else(|| io::Error::from(io::ErrorKind::AddrNotAvailable))?;
+    let certs = CertificateDer::pem_file_iter(&options.cert)?.collect::<Result<Vec<_>, _>>()?;
+    let key = PrivateKeyDer::from_pem_file(&options.key)?;
+
+    let config = rustls::ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(certs, key)?;
+    let acceptor = TlsAcceptor::from(Arc::new(config));
+
+    let listener = TcpListener::bind(&addr).await?;
+
+    loop {
+        let (stream, peer_addr) = listener.accept().await?;
+        let acceptor = acceptor.clone();
+
+        let fut = async move {
+            let client_stream = acceptor.accept(stream).await?;
+            let mut target_stream = TcpStream::connect(target_addr).await?;
+
+            let (mut client_reader, mut client_writer) = split(client_stream);
+            let (mut target_reader, mut target_writer) = target_stream.split();
+
+            let (client_to_target, target_to_client) = tokio::join!(
+                copy(&mut client_reader, &mut target_writer),
+                copy(&mut target_reader, &mut client_writer)
+            );
+            client_to_target?;
+            target_to_client?;
+
+            Ok(()) as io::Result<()>
+        };
+
+        tokio::spawn(async move {
+            if let Err(err) = fut.await {
+                eprintln!("Error handling proxy: {:?}", err);
+            }
+        });
+    }
+}
