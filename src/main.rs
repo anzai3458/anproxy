77a83ae24da57feb61267b511e8dfff1a5ce02cd
Junan Lu <anplus@foxmail.com>
2025-03-02 11:32:57 +0800

    Add X-Forwarded-xx header

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ use std::net::ToSocketAddrs;
 use std::path::PathBuf;
 
 use argh::FromArgs;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper_util::rt::TokioIo;
 use rustls::pki_types::pem::PemObject;
 use rustls::pki_types::{CertificateDer, PrivateKeyDer};
 use std::error::Error as StdError;
 use std::sync::Arc;
-use tokio::io::{copy, split, AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio_rustls::{rustls, TlsAcceptor};
+
 
 /// Simple https reverse proxy
 #[derive(FromArgs)]
@@ -61,15 +64,30 @@ async fn main() -> Result<(), Box<dyn StdError + Send + Sync + 'static>> {
 
         let fut = async move {
             let client_stream = acceptor.accept(stream).await?;
-            let mut target_stream = TcpStream::connect(target_addr).await?;
+            let client_io = TokioIo::new(client_stream);
 
-            let (mut client_reader, mut client_writer) = split(client_stream);
-            let (mut target_reader, mut target_writer) = target_stream.split();
+            let service = service_fn(move |mut req| async move {
+                let target_stream = TcpStream::connect(target_addr).await.unwrap();
+                let target_io = TokioIo::new(target_stream);
+                let (mut sender, conn) = hyper::client::conn::http1::handshake(target_io).await?;
+                tokio::task::spawn(async move {
+                    if let Err(err) = conn.await {
+                        println!("Connection failed: {:?}", err);
+                    }
+                });
+                req.headers_mut().insert("X-Forwarded-For", peer_addr.to_string().parse().unwrap());
+                req.headers_mut().insert("X-Forwarded-Proto", "https".parse().unwrap());
+                sender.send_request(req).await
+            });
 
-            tokio::select!(
-                res = copy(&mut client_reader, &mut target_writer) => res?,
-                res = copy(&mut target_reader, &mut client_writer) => res?
-            );
+            tokio::task::spawn(async move {
+                if let Err(err) = http1::Builder::new()
+                    .serve_connection(client_io, service)
+                    .await
+                {
+                    println!("Failed to serve the connection: {:?}", err);
+                }
+            });
 
             Ok(()) as io::Result<()>
         };
